811e55e9c77e53eacc75c459a912de3e5d3c9906
PythonNoobCodes <43916669+PythonNoobCodes@users.noreply.github.com>
2018-10-06 16:51:59 -0400

    Update NN.py

--- NN.py
+++ NN.py
@@ -29,9 +29,8 @@ y = HAR['lable']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y)
 scaler = StandardScaler()
-# Fit only to the training data
 scaler.fit(X_train)
-# Now apply the transformations to the data:
+
 X_train = scaler.transform(X_train)
 X_test = scaler.transform(X_test)
 
