ce04f14d989e3843cf4a64580996f5e35f9dc6a0
PythonNoobCodes <43916669+PythonNoobCodes@users.noreply.github.com>
2018-10-06 17:00:16 -0400

    Update NN.py

--- NN.py
+++ NN.py
@@ -35,6 +35,17 @@ X_train = scaler.transform(X_train)
 X_test = scaler.transform(X_test)
 
 mlp = MLPClassifier(max_iter=300)
+
+'''
+MLPClassifier(hidden_layer_sizes=(100, ), activation=’relu’, solver=’adam’, 
+alpha=0.0001, batch_size=’auto’, learning_rate=’constant’, 
+learning_rate_init=0.001, power_t=0.5, max_iter=200, shuffle=True, 
+random_state=None, tol=0.0001, verbose=False, warm_start=False, 
+momentum=0.9, nesterovs_momentum=True, early_stopping=False, 
+validation_fraction=0.1, beta_1=0.9, beta_2=0.999, epsilon=1e-08, 
+n_iter_no_change=10)[source]
+'''
+
 mlp.fit(X_train,y_train)
 
 predictions = mlp.predict(X_test)
